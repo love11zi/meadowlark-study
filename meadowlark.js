5359934ff564496fec476b7695113bab6077f232
linyang <linyang@tuniu.com>
2017-08-17 08:55:19 +0800

    initial commit

---
+++ meadowlark.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var hbs = require('express-hbs');
+var app = express();
+
+var fortunes = [
+    "Conquer your fears or they will conquer you.", "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.", "Whenever possible, keep it simple.",
+];
+
+app.set('port', process.env.PORT || 3000);
+
+app.engine('hbs', hbs.express4({
+    defaultLayout: __dirname + '/views/layouts/main'
+}));
+
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+app.get('/about', function(req, res){
+    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', {fortune: randomFortune});
+});
+
+app.use(function (req, res) {
+    res.status('404');
+    res.render('404');
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status('500');
+    res.render('500');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Express started on http://localhost:' + 
+        app.get('port') + '; press Ctrl+C to terminate');
+}); 
